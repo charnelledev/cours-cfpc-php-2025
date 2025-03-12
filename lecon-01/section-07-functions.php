4de11b3102e65902027b5183e54541ac2080b6c9
charnelledev <kengnemaiva54@gmail.com>
2025-03-12 16:39:15 +0100

    arrow function

--- lecon-01/section-07-functions.php
+++ lecon-01/section-07-functions.php
@@ -15,3 +15,8 @@ function sum ($a,$b):mixed
     return $a + $b;
 }
 echo sum(a:4, b:6). '<br>';
+
+function sumAll(...$nums):mixed  {
+    return array_reduce(array: $nums,callback: fn($carry, $n):float int => $carry +  0);
+   }
+   echo "Sum of numbers 1,2,3,4,5: " . sumAll(nums:1, 2, 3, 4, 5) . "<br>";
