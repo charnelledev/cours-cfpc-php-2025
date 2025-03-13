85243850cf847b0d17ac2db52f8acacfd9798a34
charnelledev <kengnemaiva54@gmail.com>
2025-03-13 12:22:49 +0100

    post

--- lecon-02/gestion-fom.php
+++ lecon-02/gestion-fom.php
@@ -1,36 +1,37 @@
 <?php
 
-// if (isset($_POST['create'])){
-//     // echo"Formulaire creer avec succes! ";
-//     $nom_student = $_POST ['nom'];
-//     echo "Nom:  $nom_student". '<br>';
+if (isset($_POST['create'])){
+    // echo"Formulaire creer avec succes! ";
+    $nom_student = $_POST ['nom'];
+    echo "Nom:  $nom_student". '<br>';
 
-//     $prenom_student = $_POST ['prenom'];
-//     echo "Prenom:  $prenom_student".'<br>';
+    $prenom_student = $_POST ['prenom'];
+    echo "Prenom:  $prenom_student".'<br>';
 
-//     $email_student = $_POST ['mail'];
-//     echo "Email:  $email_student";
-// }
+    $email_student = $_POST ['mail'];
+    echo "Email:  $email_student".'<br>';
+}
 
 // if (empty($_POST['nom'])){
-//     echo "veillez saisir votre nom.";
+//     echo "veillez saisir votre nom". '<br>';
 // }
 // $email = $_POST['mail'];
-// if(!filter_var($$email, FILTER_VALIDATE_EMAIL)) {
-//     echo "veillez saisir une adresse email valide.";
+// if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//     echo "veillez saisir une adresse email valide". '<br>';
 // }
 
 
-if (isset($_POST['create'])) {
-  
-  if (empty($_POST['nom'])) {
-    echo "Le nom est obligatoire <br>";
-  } else {
-    $nom_student = $_POST['nom'];
+$nom_student =$_POST['nom'];
+$prenom_student =$_POST['prenom'];
+$email_student =$_POST['mail'];
+$message ="";
 
-    echo "Nom: $nom_student <br>";
-  } 
- 
+if (isset($_POST['create'])) {
+    if(empty($nom_student)|| empty($prenom_student)|| empty($email_student)) {
+        $message ="vaillez saisir tous les champs";
+    }else{
+        echo"Nom:$nom: $nom_student <br>";
+    }
 }
 
 
@@ -56,7 +57,11 @@ if (isset($_POST['create'])) {
     <h1 class="bg-green-500 text-center m-10 ">Formulaire
 
     </h1>
+
     <form action="" method="post" class="bg-white p-6 rounded shadow max-w-md mx-auto">
+    <div class ="bg-red-300 p-5 text-red-600 mb-8">
+    <?php echo "$message" ?>
+</div>
     <div class="mb-4">
       <input type="text" name="nom" placeholder="Nom"
         class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500">
