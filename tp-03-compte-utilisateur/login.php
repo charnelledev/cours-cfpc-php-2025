fc8b5867b08294d559d5ec16d3f27641bde67274
charnelledev <kengnemaiva54@gmail.com>
2025-04-08 12:24:06 +0100

    ajout des element du form

--- tp-03-compte-utilisateur/login.php
+++ tp-03-compte-utilisateur/login.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <title>Connexion</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gradient-to-r from-blue-400 via-cyan-400 to-teal-400 min-h-screen flex items-center justify-center">
+<body class="bg-gradient-to-r from-blue-400 via-cyan-400 to-teal-400 min-h-screen flex items-center justify-center mt-20 mb-20">
   <div class="bg-white p-10 rounded-2xl shadow-2xl w-full max-w-md">
     <h1 class="text-3xl font-bold text-center text-teal-700 mb-6">Connexion</h1>
     <form class="space-y-5">
@@ -35,6 +35,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       </div>
       <div>
         <label class="block mb-1 text-sm font-medium text-gray-700">Mot de passe</label>
+        <input type="password" class="w-full px-4 py-2 border rounded-xl shadow-sm focus:ring-2 focus:ring-teal-500 outline-none">
+      </div>
+      <div>
+        <label class="block mb-1 text-sm font-medium text-gray-700">confirmation du  passe</label>
         <input type="password" class="w-full px-4 py-2 border rounded-xl shadow-sm focus:ring-2 focus:ring-teal-500 outline-none">
       </div>
       <div class="flex items-center justify-between">
